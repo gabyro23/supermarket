01eda005d1297a951b19cdcc2a1ec4c8faccd213
Gabriela <gaby.ro23@yahoo.com>
2025-04-18 01:31:52 +0200

    actualizacion de step 1

--- keepaneye.js
+++ keepaneye.js
@@ -1,3 +1,14 @@
-let alimentos
-let precios
-let supermercados
+let format
+let drinks
+let amount
+
+function start() {
+    alert("Clic on the start button '")
+}
+
+
+function selectFormat() {
+    alert("Selecciona el formato de tu bebida")
+}
+let botonSeleccionarFormato = document.getElementById('format')
+botonSeleccionarFormato.addEventListener('click', selectFormat)
